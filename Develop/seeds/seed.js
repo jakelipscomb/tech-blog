ac3fc41b7d8e386f749d5e7a49823015e7743a02
jakelipscomb <jlipscombtx95@gmail.com>
2023-08-07 22:14:56 -0500

    initial start with code from previous classwork. added Comment and Post files to seeds folder. added Comment.js and Post.js to models folder with work in progress code for proper pathing

---
+++ Develop/seeds/seed.js
@@ -0,0 +1,33 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const commentData = require('./comment.json');
+const postData = require('./post.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.post.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+  
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
