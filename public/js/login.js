cbb24c2ed4d6d1d2f99f4881f60dad99821c40c6
Jake Lipscomb <jlipscombtx95@gmail.com>
2023-09-26 11:49:38 -0500

    added js folder in public with login, signin, and newPost files. formatted the main and login handlebars within views and added signup as a redirect for user login.

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value;
+    const password = document.querySelector('#password-login').value;
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        // Handle successful login, e.g., redirect
+        document.location.replace('/api/users/dashboard');
+        // alert('Login successful');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener('submit', loginFormHandler);
+  });
+  
---
+++ public/js/signIn.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const displayMessage = (message, className) => {
+      const messageElement = document.createElement('p');
+      messageElement.textContent = message;
+      messageElement.classList.add(className);
+      document.querySelector('.card').appendChild(messageElement);
+    };
+  
+    const signupFormHandler = async (event) => {
+      event.preventDefault();
+  
+      const emailInput = document.querySelector('#email-signup');
+      const passwordInput = document.querySelector('#password-signup');
+  
+      const email = emailInput.value;
+      const password = passwordInput.value;
+  
+      if (email && password) {
+        try {
+          const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          
+          if (response.ok) {
+            const responseData = await response.json();
+            displayMessage(responseData.message, 'success-message');
+            setTimeout(() => {
+              window.location.href = '/login';
+            }, 1000); // Redirect after 1 second (adjust the delay as needed)
+          } else {
+            const responseData = await response.json();
+            displayMessage(responseData.error, 'error-message');
+          }
+        } catch (err) {
+          console.error('Error during signup:', err);
+          displayMessage('An error occurred. Please try again later.', 'error-message');
+        }
+      }
+    };
+  
+    const signupForm = document.querySelector('#signup-form');
+    if (signupForm) {
+      signupForm.addEventListener('submit', signupFormHandler);
+    }
+  });
+  
